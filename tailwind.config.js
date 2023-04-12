5f21b9a5363e6d7fb4b96b6d3475b1a819a32fca
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-04-12 11:04:32 -0700

    add colors

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': '#5693ba',
+        'mid-blue': '#2278b8',
+        'dark-blue': '#0e3458',
+        warning: '#d34730',
+        'dark-grey': '#494c4e',
+        'mid-grey': '#6d6767',
+        'off-black': '#322d2d',
+      },
+    },
+  },
+  plugins: [],
+}
